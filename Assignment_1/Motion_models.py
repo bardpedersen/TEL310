395e1cc8d683e547b1255f16df6ced6d46bf69bd
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2023-10-17 14:11:27 +0200

    updated

--- Assignment_1/Motion_models.py
+++ Assignment_1/Motion_models.py
@@ -14,15 +14,11 @@ alpha1 to alpha6 are robot-specific motion error parameters
 
 It returns the probability of the final position given the initial position, the control input, the time step and the motion error parameters.
 """
-def motion_model_velocity(x_t, u_t, x_t_1, delta_t, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6):
-    x_ = x_t[0] # x'
-    y_ = x_t[1] # y'
-    theta_ = x_t[2] # θ'
-    v = u_t[0] # v
-    w = u_t[1] # w
-    x = x_t_1[0] # x
-    y = x_t_1[1] # y
-    theta = x_t_1[2] # θ
+def motion_model_velocity(x_t, u_t, x_t_1, delta_t, alpha):
+    x_, y_, theta_ = x_t # x', y', θ'
+    v, w = u_t # v, w
+    x, y, theta = x_t_1 # x, y, θ
+    alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = alpha # alpha1 to alpha6
 
     mu = 1 / 2 * ((x - x_) * math.cos(theta) + (y - y_) * math.sin(theta)) / ((y - y_) * math.cos(theta) - (x - x_) * math.sin(theta))
     
@@ -50,9 +46,7 @@ Algorithm: Probability Normal Distribution
 Computing densities of a zero-centered normal distribution with variance b2
 """
 def prob_normal_distribution(a, b_2):
-    #return np.random.normal(a, math.sqrt(b_2))
     return (1/math.sqrt(2 * math.pi * b_2)) * math.exp(-0.5 * ((a ** 2)/b_2))
-
 
 
 """
@@ -73,12 +67,10 @@ alpha1 to alpha6 are the parameters of the motion noise
 
 It returns a generated position that is a posibiltiy with the given initial position, the control input, the time step and the motion error parameters.
 """
-def sample_model_velocity(u_t, x_t_1, delta_t, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6):
-    v = u_t[0] # v
-    w = u_t[1] # w
-    x = x_t_1[0] # x
-    y = x_t_1[1] # y
-    theta = x_t_1[2] # θ
+def sample_model_velocity(u_t, x_t_1, delta_t, alpha):
+    v, w = u_t # v, w
+    x, y, theta = x_t_1 # x, y, θ
+    alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = alpha # alpha1 to alpha6
 
     # Sample noise from a normal distribution with parameters influenced by velocity and acceleration
     hat_v = v + sample_normal_distribution(alpha1 * v**2 + alpha2 * w**2) 
@@ -101,7 +93,8 @@ Algorithm: Sample Normal Distribution
 Generates sampling from (approximate) normal distributions with zero mean and variance b2
 """
 def sample_normal_distribution(b_2):
-    return sum(random.uniform(-math.sqrt(b_2), math.sqrt(b_2)) for _ in range(12)) / 2
+    b = math.sqrt(b_2)
+    return sum(random.uniform(-b, b) for _ in range(12)) / 2
 
 
 """
@@ -127,28 +120,19 @@ alpha1 to alpha6 are robot-specific motion error parameters
 It returns the probability of the final position given the initial position, the control input and the motion error parameters.
 p(x_t|x_t_1, u_t)
 """
-def motion_model_odometry(x_t, u_t, x_t_1, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6):
-    hat_x_t_1 = u_t[0] # [x_t_1] = x̂, ŷ, θ̂
-    hat_x_t = u_t[1] # [x_t] = x̂', ŷ', θ̂'
-
-    hat_x_ = hat_x_t[0] # x̂'
-    hat_y_ = hat_x_t[1] # ŷ'
-    hat_theta_ = hat_x_t[2] # θ̂'
-    hat_x = hat_x_t_1[0] # x̂
-    hat_y = hat_x_t_1[1] # ŷ
-    hat_theta = hat_x_t_1[2] # θ̂
+def motion_model_odometry(x_t, u_t, x_t_1, alpha):
+    hat_x_t_1, hat_x_t = u_t # [x_t_1] = x̂, ŷ, θ̂,  [x_t] = x̂', ŷ', θ̂'
+    hat_x_, hat_y_, hat_theta_ = hat_x_t # x̂', ŷ', θ̂'
+    hat_x, hat_y, hat_theta = hat_x_t_1 # x̂, ŷ, θ̂
+    alpha1, alpha2, alpha3, alpha4 = alpha # alpha1 to alpha4
 
     # Calculate the differences in orientation and translation based on estimated states
     delta_rot1 = math.atan2(hat_y_ - hat_y, hat_x_ - hat_x) - hat_theta
-    delta_trans = math.sqrt((hat_x_ - hat_x)**2 + (hat_y_ - hat_y)**2)
+    delta_trans = math.sqrt((hat_x - hat_x_)**2 + (hat_y - hat_y_)**2)
     delta_rot2 = hat_theta_ - hat_theta - delta_rot1
     
-    x_ = x_t[0] # x'
-    y_ = x_t[1] # y'
-    theta_ = x_t[2] # θ'
-    x = x_t_1[0] # x   
-    y = x_t_1[1] # y
-    theta = x_t_1[2] # θ
+    x_, y_, theta_ = x_t # x', y', θ'
+    x, y, theta = x_t_1 # x, y, θ
 
     # Calculate the differences in orientation and translation based on true states
     hat_delta_rot1 = math.atan2(y_ - y, x_ - x) - theta
@@ -158,7 +142,7 @@ def motion_model_odometry(x_t, u_t, x_t_1, alpha1, alpha2, alpha3, alpha4, alpha
     # Calculate probabilities using the difference values and noise parameters
     p1 = prob_normal_distribution(delta_rot1 - hat_delta_rot1, alpha1*hat_delta_rot1**2 + alpha2*hat_delta_trans**2)
     p2 = prob_normal_distribution(delta_trans - hat_delta_trans, alpha3*hat_delta_trans**2 + alpha4*hat_delta_rot1**2 + alpha4*hat_delta_rot2**2)
-    p3 = prob_normal_distribution(delta_rot2 - hat_delta_rot2, alpha5*hat_delta_rot2**2 + alpha6*hat_delta_trans**2)  
+    p3 = prob_normal_distribution(delta_rot2 - hat_delta_rot2, alpha1*hat_delta_rot2**2 + alpha2*hat_delta_trans**2)  
     return p1 * p2 * p3 # p(x_t|x_t_1, u_t)
 
 
@@ -171,29 +155,23 @@ where [x_t_1] = x̂, ŷ, θ̂ is the start position for the odom and
 
 x_t_1 = [x, y, θ]T , this is the initial position
 alpha1 to alpha6 are robot-specific motion error parameters
-"""
-def sample_model_odometry(u_t, x_t_1, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6):
-    hat_x_t_1 = u_t[0] # [x_t_1] = x̂, ŷ, θ̂
-    hat_x_t = u_t[1] # [x_t] = x̂', ŷ', θ̂'
-    hat_x_ = hat_x_t[0] # x̂'
-    hat_y_ = hat_x_t[1] # ŷ'
-    hat_theta_ = hat_x_t[2] # θ̂'
-    hat_x = hat_x_t_1[0] # x̂
-    hat_y = hat_x_t_1[1] # ŷ
-    hat_theta = hat_x_t_1[2] # θ̂
-    x = x_t_1[0] # x
-    y = x_t_1[1] # y
-    theta = x_t_1[2] # θ
+"""            
+def sample_model_odometry(u_t, x_t_1, alpha):
+    hat_x_t_1, hat_x_t = u_t # [x_t_1] = x̂, ŷ, θ̂, [x_t] = x̂', ŷ', θ̂'
+    hat_x_, hat_y_, hat_theta_ = hat_x_t # x̂', ŷ', θ̂'
+    hat_x, hat_y, hat_theta = hat_x_t_1# x̂, ŷ, θ̂
+    x , y, theta = x_t_1 # x, y, θ
+    alpha1, alpha2, alpha3, alpha4 = alpha # alpha1 to alpha4
 
     # Calculate differences in orientation and translation based on estimated states
     delta_rot1 = math.atan2(hat_y_ - hat_y, hat_x_ - hat_x) - hat_theta
-    delta_trans = math.sqrt((hat_x_ - hat_x)**2 + (hat_y_ - hat_y)**2)
+    delta_trans = math.sqrt((hat_x - hat_x_)**2 + (hat_y - hat_y_)**2)
     delta_rot2 = hat_theta_ - hat_theta - delta_rot1
 
     # Introduce noise to the differences using sampled values from a normal distribution
     hat_delta_rot1 = delta_rot1 - sample_normal_distribution(alpha1 * delta_rot1**2 + alpha2 * delta_trans**2)
     hat_delta_trans = delta_trans - sample_normal_distribution(alpha3 * delta_trans**2 + alpha4 * (delta_rot1**2 + delta_rot2**2))
-    hat_delta_rot2 = delta_rot2 - sample_normal_distribution(alpha5 * delta_rot2**2 + alpha6 * delta_trans**2)
+    hat_delta_rot2 = delta_rot2 - sample_normal_distribution(alpha1 * delta_rot2**2 + alpha2 * delta_trans**2)
     
     # Update the estimated state with the noisy differences
     x_ = x + hat_delta_trans * math.cos(theta + hat_delta_rot1)
--- Assignment_1/Motion_models_test.py
+++ Assignment_1/Motion_models_test.py
@@ -7,10 +7,6 @@ import Motion_models
 
 """
 For testing the code
-
-
-Porblems when alpha values are too small
-alpha = [0.001, 0.001, 0.001, 0.001, 0.001, 0.001]
 """
 
 # Testing the probability normal distribution
@@ -64,7 +60,7 @@ def test_motion_model_velocity():
     t = 2 # Time step
     test_ut = [1, math.pi/8] # Control input
     alpha = [0.15, 0.15, 0.15, 0.15, 0.15, 0.15] # Motion error parameters
-    print(Motion_models.motion_model_velocity(test_xt, test_ut, test_xt_1, t, alpha[0], alpha[1], alpha[2], alpha[3], alpha[4], alpha[5]))
+    print(Motion_models.motion_model_velocity(test_xt, test_ut, test_xt_1, t, alpha))
 
 
 # Testing the motion model with odometry
@@ -72,8 +68,8 @@ def test_motion_model_odometry():
     test_xt = [1.8, 0.75, math.pi/4]  # Final position
     test_xt_1 = [0, 0, 0]  # Start position
     test_ut = [[0, 0, 0], [1.85, 0.77, 0.81]] # Control input [x_t_1, x_t] [x_t_1] = x, y, θ
-    alpha = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05]
-    print(Motion_models.motion_model_odometry(test_xt, test_ut, test_xt_1, alpha[0], alpha[1], alpha[2], alpha[3], alpha[4], alpha[5]))
+    alpha = [0.05, 0.05, 0.05, 0.05]
+    print(Motion_models.motion_model_odometry(test_xt, test_ut, test_xt_1, alpha))
 
 
 # Testing the sample model with velocity
@@ -83,7 +79,7 @@ def test_sample_model_velocity():
     test_xt_1 = [0, 0, 0]  # Start position
     t = 2 # Time step
     alpha = [0.001, 0.001, 0.001, 0.001, 0.001, 0.001] # Motion error parameters
-    x_positions, y_positions, z_positions = zip(*(Motion_models.sample_model_velocity(test_ut, test_xt_1, t, alpha[0], alpha[1], alpha[2], alpha[3], alpha[4], alpha[5]) for _ in range(100000)))
+    x_positions, y_positions, z_positions = zip(*(Motion_models.sample_model_velocity(test_ut, test_xt_1, t, alpha) for _ in range(100000)))
     plt.scatter(x_positions, y_positions, c='b', marker='o')
     plt.scatter(0,0, c='r', marker='o')
     plt.scatter(test_xt[0],test_xt[1], c='r', marker='o')
@@ -95,13 +91,13 @@ def test_sample_model_odometry():
     test_ut = [[0, 0, 0], [1.85, 0.77, 0.81]] # Control input [x_t_1, x_t] [x_t_1] = x, y, θ
     test_xt = [1.8, 0.75, math.pi/4]  # Final position
     test_xt_1 = [0, 0, 0]  # Start position
-    alpha = [0.001, 0.001, 0.001, 0.001, 0.001, 0.001] # Motion error parameters
-    x_positions, y_positions, z_positions = zip(*(Motion_models.sample_model_odometry(test_ut, test_xt_1, alpha[0], alpha[1], alpha[2], alpha[3], alpha[4], alpha[5]) for _ in range(100000)))
+    alpha = [0.001, 0.001, 0.001, 0.001] # Motion error parameters
+    x_positions, y_positions, z_positions = zip(*(Motion_models.sample_model_odometry(test_ut, test_xt_1, alpha) for _ in range(100000)))
     plt.scatter(x_positions, y_positions, c='b', marker='o')
     plt.scatter(0,0, c='r', marker='o')
     plt.scatter(test_xt[0],test_xt[1], c='r', marker='o')
     plt.show()
-
+      
 
 if __name__ == "__main__":
     test_prob_normal_distribution()
