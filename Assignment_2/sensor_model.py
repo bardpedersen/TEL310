7b008987f8b1eefb2fda7f83ab140f0a1987265d
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2023-10-22 16:12:09 +0200

    update task2

--- Assignment_2/sensor_model.py
+++ Assignment_2/sensor_model.py
@@ -2,6 +2,7 @@
 
 import math
 import scipy.stats as stats
+import numpy as np
 
 """
 """
@@ -13,12 +14,12 @@ def prob_normal_distribution(a, b, c_2):
 """
 def p_hit(ztk, ztk_star, zmax, sigma_hit):
     if 0 <= ztk <= zmax:
-        N = prob_normal_distribution(ztk, ztk_star, sigma_hit)
+        N = prob_normal_distribution(ztk, ztk_star, sigma_hit**2)
         cdf = (stats.norm.cdf([0, zmax], loc=ztk_star, scale=sigma_hit))
         cdf = cdf[1] - cdf[0]
         return cdf**(-1) * N 
-    else:
-        return 0
+    
+    return 0
 
 """
 """
@@ -26,29 +27,28 @@ def p_short(ztk, ztk_star, lambda_short):
     if 0 <= ztk <= ztk_star:
         n = 1/(1-math.exp(-lambda_short*ztk_star))
         return n * lambda_short*math.exp(-lambda_short*ztk)
-    else:
-        return 0
+    
+    return 0
 
 """
 """
 def p_max(ztk, zmax):
     if ztk == zmax:
         return 1
-    else:
-        return 0
+    
+    return 0
     
 """
 """
 def p_rand(ztk, zmax):
     if 0 <= ztk <= zmax:
         return 1/zmax
-    else:
-        return 0
     
-"""
-"""
+    return 0
+    
+
+
 def ray_casting(ztk, xt, m):
-    a = ztk + xt + m
     return None
 
 
@@ -57,17 +57,11 @@ zt = range scan in the range of [0;zmax]
 xt = robot pose
 m = map
 """
-def beam_range_finder_model(zt, xt, m, Theta): 
+def beam_range_finder_model(zt, xt, m, theta): 
+    z_hit, z_short, z_max, z_rand, sigma_hit, lambda_short = theta
     q = 1.0
-    z_hit   = Theta(1)
-    z_short = Theta(2)
-    z_max   = Theta(3)
-    z_rand  = Theta(4)
-    sigma_hit = Theta(5)
-    lambda_short = Theta(6)
 
-    for k in range(len(zt)):
-        ztk = zt[k]
+    for ztk in zt:
         # Compute the expected measurement for the current pose and map
         ztk_star = ray_casting(ztk, xt, m)
         
@@ -82,3 +76,85 @@ def beam_range_finder_model(zt, xt, m, Theta):
         q *= p
     
     return q
+
+
+def learn_intrinsic_parameters(Z, X, m, theta):
+    z_hit, z_short, z_max, z_rand, sigma_hit, lambda_short = theta
+    ei_hit = []
+    ei_short = []
+    ei_max = []
+    ei_rand = []
+
+    for zi in Z:
+        zi_star = None
+        n = 1/(p_hit(zi, zi_star, z_max, sigma_hit) + p_short(zi, zi_star, lambda_short) + p_max(zi, z_max) + p_rand(zi, z_max))
+        ei_hit.append(n * p_hit(zi, zi_star, z_max, sigma_hit))
+        ei_short.append(n * p_short(zi, zi_star, lambda_short))
+        ei_max.append(n * p_max(zi, z_max))
+        ei_rand.append(n * p_rand(zi, z_max))
+
+    z_hit = sum(ei_hit) / abs(Z)
+    z_short = sum(ei_short) / abs(Z)
+    z_max = sum(ei_max) / abs(Z)
+    z_rand = sum(ei_rand) / abs(Z)
+
+    sigma_hit = math.sqrt(sum(ei_hit * (zi - zi_star)**2 for zi in Z) / sum(ei_hit))
+    lambda_short = sum(ei_short) / sum(ei_short * zi for zi in Z)
+
+    return z_hit, z_short, z_max, z_rand, sigma_hit, lambda_short
+
+
+def likelihood_field_range_finder_model(zt, xt, m, Theta):
+    z_hit, z_rand, z_max, sigma_hit = Theta
+    x, y, theta = xt
+
+    q = 1
+    xk_sens = None  # Vet ikke
+    yk_sens = None  # vet ikke
+    theta_k_sens = None  # vet ikke
+    x_ = None # vet ikke
+    y_ = None # vet ikke
+
+    for ztk in zt:
+        if ztk != z_max:
+            xztk = x + xk_sens * np.cos(theta) - yk_sens * np.sin(theta) + ztk*np.cos(theta + theta_k_sens)
+            yztk = y + yk_sens * np.cos(theta) + xk_sens * np.sin(theta) + ztk*np.sin(theta + theta_k_sens)
+
+            dist = min(np.sqrt((xztk - x_)**2 + (yztk - y_)**2), z_max)
+
+            prob_hit = stats.norm.pdf(dist, loc=0, scale=sigma_hit)
+            q *= (z_hit * prob_hit + z_rand / z_max)
+    
+    return q
+
+
+def landmark_model_known_correspondence(fit, cit, xt, m):
+    x, y, theta = xt
+    rit, thetait, sit = fit
+
+    sigma_r = None
+    sigma_theta = None
+    sigma_s = None
+    sj = None
+
+
+    j = cit
+    r_hat = np.sqrt((m[j][x] - x)**2 + (m[j][y] - y)**2)
+    theta_hat = np.arctan2(m[j][y] - y, m[j][x] - x)
+    return stats.norm.pdf(rit - r_hat, loc=0, scale=sigma_r) * stats.norm.pdf(thetait - theta_hat, loc=0, scale=sigma_theta) * stats.norm.pdf(sit - sj, loc=0, scale=sigma_s)
+ 
+
+def  sample_landmark_model_known_correspondence(fit, cit, m):
+    rit, thetait, sit = fit
+
+    sigma_r = None
+    sigma_theta = None
+
+    j = cit
+    gamma_hat = np.random.uniform(0, 2*np.pi)
+    r_hat = rit + np.random.normal(0, sigma_r)
+    theta_hat = thetait + np.random.normal(0, sigma_theta)
+    x = m[j][x] + r_hat * np.cos(gamma_hat)
+    y = m[j][y] + r_hat * np.sin(gamma_hat)
+    theta = gamma_hat - np.pi - theta_hat
+    return x, y, theta
--- Assignment_2/sensor_model_test.py
+++ Assignment_2/sensor_model_test.py
@@ -1,33 +1,69 @@
 #!/usr/bin/env/python3
 
 import sensor_model
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def test_p_hit():
-    ztk = 1
+    list_av_values = []
     zmax = 10
-    ztk_star = 1
+    z = np.linspace(0, zmax, 1000)
+    ztk_star = 5
     sigma_hit = 1
-    print(sensor_model.p_hit(ztk, zmax, ztk_star, sigma_hit))
+    for ztk in z:
+        list_av_values.append(sensor_model.p_hit(ztk, ztk_star, zmax, sigma_hit))
+    plt.plot(z, list_av_values)
+    plt.show()
 
 def test_p_short():
-    ztk = 1
-    ztk_star = 1
+    list_av_values = []
+    zmax = 10
+    z = np.linspace(0, zmax, 1000)
+    ztk_star = 5
     lambda_short = 1
-    print(sensor_model.p_short(ztk, ztk_star, lambda_short))
+    for ztk in z:
+        list_av_values.append(sensor_model.p_short(ztk, ztk_star, lambda_short))
+    plt.plot(z, list_av_values)
+    plt.show()
 
 def test_p_max():
-    ztk = 10
+    list_av_values = []
     zmax = 10
-    print(sensor_model.p_max(ztk, zmax))
+    z = np.linspace(0, zmax, 1000)
+    for ztk in z:
+        list_av_values.append(sensor_model.p_max(ztk, zmax))
+    plt.plot(z, list_av_values)
+    plt.show()
 
 def test_p_rand():
-    ztk = 10
+    list_av_values = []
     zmax = 10
-    print(sensor_model.p_rand(ztk, zmax))
+    z = np.linspace(0, zmax, 1000)
+    for ztk in z:
+        list_av_values.append(sensor_model.p_rand(ztk, zmax))
+    plt.plot(z, list_av_values)
+    plt.show()
 
+def test_all_p():
+    list_av_values = []
+    zmax = 10
+    z = np.linspace(0, zmax, 1000)
+    ztk_star = 5
+    sigma_hit = 1
+    lambda_short = 1
+    for ztk in z:
+        value = 0
+        value += (sensor_model.p_hit(ztk, ztk_star, zmax, sigma_hit))
+        value += (sensor_model.p_short(ztk, ztk_star, lambda_short))
+        value += (sensor_model.p_max(ztk, zmax))
+        value += (sensor_model.p_rand(ztk, zmax))
+        #print(value)
+        list_av_values.append(value/4)
+    plt.plot(z, list_av_values)
+    plt.show()  
 
 test_p_hit()
 test_p_short()
 test_p_max()
 test_p_rand()
+test_all_p()
