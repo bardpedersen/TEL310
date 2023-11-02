6931bed0a150f928219dd4acf9cc12e3186d0f15
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2023-11-02 21:32:17 +0100

    Created plot, and first simulation

--- Assignment_3/mobile_robot_localization_1_test.py
+++ Assignment_3/mobile_robot_localization_1_test.py
@@ -5,10 +5,53 @@ import matplotlib.pyplot as plt
 import mobile_robot_localization_1 as mrl1
 
 
+def calculate_rti_phiti_sti(j_x, j_y, x, y, theta):
+    r_hat = np.sqrt((j_x - x)**2 + (j_y - y)**2)
+    theta_hat = np.arctan2(j_y - y, j_x - x) - theta
+
+    return r_hat, theta_hat
+
+
+def plot_beams(xt, m, number_of_readings, z_maxlen, zt_start=np.pi/4, zt_end=-np.pi/4, transpose = True):
+    x_list = []
+    y_list = []
+    x_list_st = []
+    y_list_st = []
+    tolerance = 0.1
+
+    x, y, theta = xt # x, y, θ (robot pose)
+    angle_inc = np.linspace(zt_start, zt_end, number_of_readings) # For lidar
+    if transpose: # Transpose the map when x is y and y is x
+        m = np.transpose(m)
+
+    for angle in angle_inc:
+        for i in range(int(z_maxlen)): # For every block in the map up to the max range
+            m_x = x + i * np.cos(angle + theta) 
+            m_y = y + i * -np.sin(angle + theta) # -sin is only nessasary when the map is wrong // else sin
+
+            if np.sqrt((m_x - x)**2 + (m_y - y)**2) > z_maxlen: # If the distance to the block is larger than the max range
+                break
+
+            if -tolerance < angle < tolerance:
+                x_list_st.append(m_x)
+                y_list_st.append(m_y)
+            else:
+                x_list.append(m_x)
+                y_list.append(m_y)
+            if m_x < 0 or m_y < 0 or m_x > 16 or m_y > 16: # If the block is outside the map
+                break
+
+            if m[round(m_x)][round(m_y)] == 1: # If the block is occupied
+                break # Return the distance to the block
+
+    return x_list, y_list, x_list_st, y_list_st 
+
+
 def EKF_localization_known_correspondences_test():
-    time = 4
+    time = 3
     μt_1 = [8, 8, 0]
-    μt_1_1 = μt_1.copy()
+    μt_1_1 = μt_1.copy() # Create a copy of μt_1 for plotting true trajectory
+    
     Σt_1 = np.array([[0.1, 0, 0],
                     [0, 0.1, 0],
                     [0, 0, 0.1]])
@@ -20,47 +63,68 @@ def EKF_localization_known_correspondences_test():
                 [4, 0, 5], [8, 0, 6], [12, 0, 7], [16, 0, 8], 
                 [16, 4, 9], [16, 8, 10], [16, 12, 11], [16, 16, 12],
                 [12, 16, 13], [8, 16, 14], [4, 16, 15]] # [x, y, signature]
-    plt.imshow(m, cmap='gray')
-    for i in landmarks:
-        plt.plot(i[0], i[1], 'ro')
     
+    
+    plt.imshow(m, cmap='gray')
+    plot_landmarks = []
+    plot_estimated= []
+    plot_true = []
+    for i in landmarks:
+        plot_landmarks.append([i[0], i[1]])
+
     """
     Need to define better u, z, and c
     """
-    u = np.array([[1, 0], 
-                  [1, np.pi/2],
-                [1, np.pi/2],
-                [1, np.pi/2]]) # [vt, wt]
-    
-    z = np.array([[[4, np.pi/2, 0], [8, np.pi/2, 1], [12, np.pi/2, 2], [16, np.pi/2, 3]],
-                [[4, np.pi/2, 0], [8, np.pi/2, 1], [12, np.pi/2, 2], [16, np.pi/2, 3]],
-                [[4, np.pi/2, 0], [8, np.pi/2, 1], [12, np.pi/2, 2], [16, np.pi/2, 3]],
-                [[4, np.pi/2, 0], [8, np.pi/2, 1], [12, np.pi/2, 2], [16, np.pi/2, 3]]]) # [[rti, phiti, sti], ...]
 
-    c = np.array([[[0, 16, 0], [0, 12, 1], [0, 8, 2], [0, 4, 3]], 
-                  [[0, 0, 4], [4, 0, 5], [8, 0, 6], [12, 0, 7]],
-                [[16, 0, 8], [16, 4, 9], [16, 8, 10], [16, 12, 11]],
-                [[16, 16, 12], [12, 16, 13], [8, 16, 14], [4, 16, 15]]]) # [[x, y, signature], ...]
+    u = np.array([[3, np.pi/4], 
+                [4, np.pi/4],
+                [4, np.pi/3]]) # [vt, wt]
+    
+    c = np.array([[[16, 16, 12], [16, 12, 11], [16, 8, 10], [16, 4, 9], [16, 0, 8]],
+                [[16, 8, 10], [16, 4, 9], [16, 0, 8], [12, 0, 7]],
+                [[16, 0, 8], [12, 0, 7]]]) # [[x, y, signature], ...]
+    
+    z = np.array([[[11, 0.78, 12], [9, 0.46, 11], [8, 0, 10], [9, -0.46, 9], [11, -0.78, 8]],
+                [[5, -0.78, 10], [6.4, -1.5, 9], [9.4, -1.8, 8], [8, -2.2, 7]],
+                [[5.6, -2.7, 8], [5.5, -3.5, 7]]]) # [[rti, phiti, sti], ...]
 
     for t in range(time):
         delta_t = 1 # Time difference between t-1 and t
         ut = u[t]
         zt = z[t]
         ct = c[t]
+
+        # For calculating rti, phiti, sti
+        #for j in ct:
+            #print(calculate_rti_phiti_sti(j[0], j[1], μt_1_1[0], μt_1_1[1], μt_1_1[2]))
+
         μt_1, Σt_1, pzt = mrl1.EKF_localization_known_correspondences(μt_1, Σt_1, ut, zt, ct, delta_t, alpha, sigma)
         
-        # For plotting estimate trajectory
-        plt.plot(μt_1[0], μt_1[1], 'go')
-
         # For plotting true trajectory
-        μt_1_1[0] = μt_1_1[0] + ut[0] * delta_t * np.cos(μt_1_1[2])
-        μt_1_1[1] = μt_1_1[1] + ut[0] * delta_t * np.sin(μt_1_1[2])
-        μt_1_1[2] = μt_1_1[2] + ut[1] * delta_t
-        plt.plot(μt_1_1[0], μt_1_1[1], 'bo')
+        plot_true.append([μt_1_1[0], μt_1_1[1]])
 
-        print("Σt_1 = ", Σt_1)
-        print("pzt = ", 0)
-    
+        # For plotting estimate trajectory
+        plot_estimated.append([μt_1[0], μt_1[1]])
+        
+        # PLotting beams
+        #x_list, y_list, x_list_st, y_list_st = plot_beams(μt_1_1, m, 2, 16)
+        #plt.plot(x_list, y_list, 'b.')
+        #plt.plot(x_list_st, y_list_st, 'g.')
+        
+        μt_1_1[0] = μt_1_1[0] + ut[0] * delta_t * np.cos(μt_1_1[2])
+        μt_1_1[1] = μt_1_1[1] + ut[0] * delta_t * -np.sin(μt_1_1[2])
+        μt_1_1[2] = μt_1_1[2] + ut[1] * delta_t
+
+        #print("Σt_1 = ", Σt_1)
+        #print("pzt = ", 0)
+
+    x_landmarks, y_landmarks = zip(*plot_landmarks)
+    x_estimated, y_estimated = zip(*plot_estimated)
+    x_true, y_true = zip(*plot_true)
+    plt.plot(x_landmarks, y_landmarks, 'ro', label='Landmarks')
+    plt.plot(x_true, y_true, 'bo', label='True trajectory')
+    plt.plot(x_estimated, y_estimated, 'go', label='Estimated trajectory')
+    plt.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
     plt.show()
 
 
@@ -70,3 +134,5 @@ def EKF_localization_known_correspondences_test():
 
 if __name__ == '__main__':
     EKF_localization_known_correspondences_test()
+
+    
