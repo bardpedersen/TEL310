549ca1513ebca04e19b53e2b2c3d3113993d5a82
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2023-11-24 23:42:37 +0100

    update

--- Assignment_4/mobile_robot_localization_2.py
+++ Assignment_4/mobile_robot_localization_2.py
@@ -8,6 +8,7 @@ import sensor_model as sm
 
 def Grid_localization(pkt_1, ut, zt, m, delta_t, alpha, Theta, z_maxlen):
     """
+    Function that implements the Grid Localization algorithm
     pkt_1 = how you want the grid to look like at time t-1
     ut = control input
     zt = sensor measurement
@@ -18,81 +19,187 @@ def Grid_localization(pkt_1, ut, zt, m, delta_t, alpha, Theta, z_maxlen):
     alpha = motion error parameters
     Theta = sensor model parameters
     z_maxlen = maximum sensor range
+
+    Returns a grid with the probability of the robot being at each cell
     """
-    n = 1
+    sense_coord = np.array([0, 0, 0])
+    zt_start = 0
+    zt_end = np.pi
     size_y = m.shape[0]/pkt_1.shape[0]
     size_x = m.shape[1]/pkt_1.shape[1]
-    xk = [size_x, size_y, np.pi/2] 
-    xi = [size_x-ut[0]-1, size_y-1, np.pi/2] 
+    xk = [size_x/2, size_y/2, np.pi/2] 
+    xi = [size_x/2 + ut[0], size_y/2 - 1, np.pi/2] 
     p_kt = np.zeros(pkt_1.shape)
-    for x in range(pkt_1.shape[0]-1):
-        for y in range(pkt_1.shape[1]-1):
-            p_bar_kt =  pkt_1[x][y] * mm.motion_model_velocity(xk, ut, xi, delta_t, alpha)
-            p_kt[x][y] = n * p_bar_kt * sm.beam_range_finder_model(zt, xk, m, Theta, z_maxlen, zt_start=0, zt_end=2*np.pi, transpose=True)
+    for x in range(pkt_1.shape[0]):
+        for y in range(pkt_1.shape[1]):
+            p_bar_kt =  pkt_1[x][y] * mm.motion_model_velocity(xk, ut, xi, delta_t, alpha) ########
+            p_kt[x][y] = p_bar_kt * sm.likelihood_field_range_finder_model(zt, xk, m, Theta, z_maxlen, sense_coord, zt_start, zt_end, transpose=True) 
             xk[1] += size_y
             xi[1] += size_y
         xi[0] += size_x
         xk[0] += size_x
         xk[1] = 0
         xi[1] = 0
+
+    #normalize p_kt
+    p_kt = p_kt/np.sum(p_kt)
     return p_kt
 
+def generate_particles(m):
+    """
+    Function that generates a random particle on the free spaces in the map m
+    m = map
+
+    Returns a random particle with the following format: [x, y, theta]
+    """
+    free_spaces = np.argwhere(m == 0)  # Get indices of free spaces
+    random_index = free_spaces[np.random.choice(free_spaces.shape[0])]  # Randomly select one index
+    random_theta = np.random.uniform(0, 2*np.pi)  # Generate a random theta
+
+    return [random_index[1], random_index[0], random_theta]
+
 def MCL(Xt_1, ut, zt, m):
-    alpha = [0.001, 0.001, 0.001, 0.001, 0.001, 0.001]
+    """
+    Function that implements the Monte Carlo Localization algorithm
+    Xt_1 = list of particles at time t-1
+    ut = control input
+    zt = sensor measurement
+    m = map
+
+    Returns a list of particles with the following format: [[x, y, theta], weight]
+    """
+    alpha = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
     theta = np.array([0.8157123986145881, 0.00235666025958796, 0.16552184295092348, 0.01640909817490046, 1.9665518618953464, 0.0029480342354130016])
     z_maxlen = 500
     sense_coord = np.array([0, 0, 0])
-    zt_start = np.pi/4
-    zt_end = -np.pi/4
+    zt_start = 0
+    zt_end = np.pi
     Xbar_t = []
 
     for m_, xt_1 in enumerate(Xt_1): # is number of particles
-        
         xt = mm.sample_model_velocity(ut, xt_1[0], 1, alpha)
-        while xt[0] < 0 or xt[0] > m.shape[1] or xt[1] < 0 or xt[1] > m.shape[0] or m[int(xt[1]), int(xt[0])] == 1:
-            xt = mm.sample_model_velocity(ut, xt_1[0], 1, alpha)
-        wt = sm.likelihood_field_range_finder_model(zt, xt, m, theta, z_maxlen, sense_coord, zt_start, zt_end, transpose=False) 
+        wt = sm.likelihood_field_range_finder_model(zt, xt, m, theta, z_maxlen, sense_coord, zt_start, zt_end, transpose=True) 
         Xbar_t.append([xt, wt])
+        
+    # Normalize the weights
+    sum_weights = sum(w for (_, w) in Xbar_t)
+    normalized_weights = [w / sum_weights for (_, w) in Xbar_t]
 
-        # if weight is high, sample more around that particle
-        if xt_1[1] > 0.1:
-            for _ in range(5):
-                xt = mm.sample_model_velocity(ut, xt_1[0], 1, alpha)
-                while xt[0] < 0 or xt[0] > m.shape[1] or xt[1] < 0 or xt[1] > m.shape[0] or m[int(xt[1]), int(xt[0])] == 1:
-                    xt = mm.sample_model_velocity(ut, xt_1[0], 1, alpha)
-                wt = sm.likelihood_field_range_finder_model(zt, xt, m, theta, z_maxlen, sense_coord, zt_start, zt_end, transpose=False) 
-                Xbar_t.append([xt, wt])
-
-        while len(Xbar_t) > len(Xt_1): # if too many particles, remove the ones with the lowest weight
-            min_weight_sublist = min(Xbar_t, key=lambda x: x[1])
-            Xbar_t.remove(min_weight_sublist)
-
-    Xbar_t = np.array(Xbar_t, dtype=object)
-    epsilon = 1e-10  # Small constant to avoid division by zero
-    Xbar_t[:, 1] = Xbar_t[:, 1] / (np.sum(Xbar_t[:, 1]) + epsilon)
-
+    # Resampling
+    indices = np.random.choice(range(len(Xt_1)), len(Xt_1), p=normalized_weights)
+    Xbar_t = [[Xbar_t[i][0], Xbar_t[i][1]] for i in indices]
     return np.array(Xbar_t)
 
 
 def Augmented_MCL(Xt_1, ut, zt, m):
-    pass
+    """
+    Function that implements the Augmented Monte Carlo Localization algorithm
+    Xt_1 = list of particles at time t-1
+    ut = control input
+    zt = sensor measurement
+    m = map
+
+    Returns a list of particles with the following format: [[x, y, theta], weight]
+    """
+    alpha = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+    theta = np.array([0.8157123986145881, 0.00235666025958796, 0.16552184295092348, 0.01640909817490046, 1.9665518618953464, 0.0029480342354130016])
+    z_maxlen = 500
+    sense_coord = np.array([0, 0, 0])
+    zt_start = 0
+    zt_end = np.pi
+    Xbar_t = []
+    Xt = []
+    Wavg = 0
+    Wslow = 0.1
+    Wfast = 0.6
+    alpha_slow = 0.001
+    alpha_fast = 0.1
+
+    for m_, xt_1 in enumerate(Xt_1): # is number of particles
+        xt = mm.sample_model_velocity(ut, xt_1[0], 1, alpha)
+        wt = sm.likelihood_field_range_finder_model(zt, xt, m, theta, z_maxlen, sense_coord, zt_start, zt_end, transpose=True) 
+        Xbar_t.append([xt, wt])
+        Wavg += wt/len(Xt_1)
+
+    # Normalize the weights
+    Wslow  += alpha_slow * (Wavg - Wslow)
+    Wfast  += alpha_fast * (Wavg - Wfast)
+
+    for m_, (xt, wt) in enumerate(Xbar_t):
+        if np.random.rand() < max(0.0, 1.0 - Wfast / Wslow):
+            Xt.append([generate_particles(m), 1/len(Xt_1)])
+        else:
+            indices = np.random.choice(range(len(Xbar_t)), len(Xbar_t), p=[Wavg / sum(w for (_, w) in Xbar_t) for _ in range(len(Xbar_t))])
+            Xt.append([Xbar_t[indices[0]][0], Xbar_t[indices[0]][1]])
+
+    return np.array(Xt)
 
 
 def KLD_Sampling_MCL(Xt_1, ut, zt, m, epsilon, delta):
-    pass
+    """
+    Function that implements the KLD Sampling Monte Carlo Localization algorithm
+    Xt_1 = list of particles at time t-1
+    ut = control input
+    zt = sensor measurement
+    m = map
+    epsilon = threshold
+    delta = threshold
+
+    Returns a list of particles with the following format: [[x, y, theta], weight]
+    """
+    alpha = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+    theta = np.array([0.8157123986145881, 0.00235666025958796, 0.16552184295092348, 0.01640909817490046, 1.9665518618953464, 0.0029480342354130016])
+    z_maxlen = 500
+    sense_coord = np.array([0, 0, 0])
+    zt_start = 0
+    zt_end = np.pi
+
+    Xt = []
+    M, Mx, k, Mx_min = 0, 1, 0, len(Xt_1)
+    H = [] # bins
+
+    while M < Mx and M < Mx_min:
+        sum_weights = sum(w for (_, w) in Xt_1)
+        weights = [w / sum_weights for (_, w) in Xt_1]
+        indices = np.random.choice(range(len(Xt_1)), len(Xt_1), p=weights)
+        Xbar_t = [[Xt_1[i][0], Xt_1[i][1]] for i in indices]
+        
+        for _, (xt, wt) in enumerate(Xbar_t):
+            xt = mm.sample_model_velocity(ut, xt, 1, alpha)
+            wt = sm.likelihood_field_range_finder_model(zt, xt, m, theta, z_maxlen, sense_coord, zt_start, zt_end, transpose=True) 
+            Xt.append([xt, wt])
+
+            for h in H:
+                if np.array_equal(h, xt):
+                    break
+            else:
+                H.append(xt)
+                k += 1
+
+                if k > 1:
+                    Z1 = np.random.normal(0, 1)
+                    Mx = (k-1)/(2*epsilon) * (1- 2/(9*(k-1)) + np.sqrt(2/(9*(k-1))) * Z1-delta)**3
+            M += 1
+    return np.array(Xt)
 
 
-def test_range_measurement(ztk, Xbart, m):
-    z_short, z_max, z_rand, z_hit = 0.1, 0.1, 0.1, 0.7 #########
-    sigma_hit = 0.1 #########
-    lambda_short = 0.1 #########
-    ztk_star = 0 #########
-    zmax = 100 #########
-    x = 0.5 #########
-    M = 100 #########
+def test_range_measurement(zt, Xbar_t, m , x):
+    """
+    Function that tests if the range measurement zt is consistent with the map m
+    zt = sensor measurement
+    Xbar_t = list of particles
+    m = map
+    x = threshold
+    
+    Returns True if the measurement is consistent, False otherwise
+    """
+    z_short, z_max, z_rand, z_hit, sigma_hit, lambda_short = 0.8157123986145881, 0.00235666025958796, 0.16552184295092348, 0.01640909817490046, 1.9665518618953464, 0.0029480342354130016
+    zmax = 500
+    p, q = 0, 0
 
-    p, q = 0
-    for m in range(M):
+    for m_, xt in enumerate(Xbar_t):
+        ztk = zt[m_]
+        ztk_star = sm.ray_casting(xt, m, 0, zmax, transpose=True)
         p  += z_short * sm.p_short(ztk, ztk_star, lambda_short)
         q  += z_hit * sm.p_hit(ztk, ztk_star, zmax, sigma_hit) + z_short * sm.p_short(ztk, ztk_star, lambda_short) + z_max * sm.p_max(ztk, zmax) + z_rand * sm.p_rand(ztk, zmax)
 
@@ -100,3 +207,4 @@ def test_range_measurement(ztk, Xbart, m):
         return True
     else:
         return False
+    
--- Assignment_4/mobile_robot_localization_2_test.py
+++ Assignment_4/mobile_robot_localization_2_test.py
@@ -8,14 +8,13 @@ import os
 from PIL import Image
 
 
+
 def Grid_localization_test():
     m = np.load('map/binary_image_cats.npy')
-    pkt_1 = np.ones((15,15)) / (15*15)
+    pkt_1 = np.ones((10,10)) / (10*10)
     ut = [10, 0]
-    zt = np.array([218.0, 182, 500, 303.0, 355.0, 496.0, 182, 206]) # exact values 
-    noise = np.random.normal(0, 2, size=len(zt)) 
-    zt += noise # add noise to the values
-    zt[2] = 500 # to not get larger than 500
+    xt = [[500, 50, np.pi/2]]
+    zt = find_z_values(xt, m)
 
     z_hit = 0.8157123986145881
     z_short = 0.00235666025958796
@@ -28,11 +27,15 @@ def Grid_localization_test():
     delta_t = 1
     alpha = [0.001, 0.001, 0.001, 0.001, 0.001, 0.001]
 
-    p_kt = mrl2.Grid_localization(pkt_1, ut, zt, m, delta_t, alpha, theta, z_maxlen)
+    p_kt = mrl2.Grid_localization(pkt_1, ut, zt[0], m, delta_t, alpha, theta, z_maxlen)
     p_kt = np.transpose(np.array(p_kt))
-    print(np.sum(p_kt))
-    plt.imshow(p_kt)
-    plt.show()
+    extent = [0, m.shape[1], m.shape[0], 0]  # [left, right, bottom, top]
+
+    plt.imshow(m, extent=extent, cmap='Greys')
+    plt.scatter(xt[0][0], xt[0][1], c='g', marker='o')
+    plt.imshow(p_kt, alpha=0.5, extent=extent, cmap='Greys')
+    plt.savefig('images/grid_localization.png')
+    plt.close()
 
 def generate_particles(number_of_particles, m):
     list_of_particles = []
@@ -48,8 +51,8 @@ def find_z_values(xt_list, m):
     zt = []
     for xt in xt_list:
         z_maxlen = 500
-        zt_start = np.pi/4
-        zt_end = -np.pi/4
+        zt_start = 0
+        zt_end = np.pi
         number_of_readings = 8
         zt1 = [sensor_model.ray_casting(xt, m, theta, z_maxlen, transpose=True) for theta in np.linspace(zt_start, zt_end, number_of_readings)]
         zt1 = np.array(zt1) 
@@ -60,15 +63,28 @@ def find_z_values(xt_list, m):
 
     return zt
 
-def create_MCL_GIF():
-    
-    filenames = MCL_test()
+def find_z_values_1(xt_list, m):
+    zt = []
+    for xt in xt_list:
+        z_maxlen = 500
+        zt_start = 0
+        zt_end = 0
+        number_of_readings = 1
+        zt1 = [sensor_model.ray_casting(xt, m, theta, z_maxlen, transpose=True) for theta in np.linspace(zt_start, zt_end, number_of_readings)]
+        zt1 = np.array(zt1) 
+        noise = np.random.normal(0, 2, size=len(zt1))  # Generate noise
+        zt1 += noise  # Add noise to zt
+        zt1 = np.minimum(zt1, z_maxlen)  # Limit maximum value
+        zt.append(zt1)
+    return zt
+
+def create_GIF(function):
     # Convert the individual plots to a GIF
-    with Image.open(filenames[0]) as im:
-        im.save("images/animation_MCL.gif", save_all=True, append_images=[Image.open(f) for f in filenames[1:]], duration=100, loop=0)
+    with Image.open(function[0]) as im:
+        im.save("images/animation_MCL.gif", save_all=True, append_images=[Image.open(f) for f in function[1:]], duration=1000, loop=0)
 
     # Remove the individual plot files
-    for f in filenames:
+    for f in function:
         os.remove(f)
 
 
@@ -77,33 +93,154 @@ def MCL_test():
     m = np.load('map/binary_image_cats.npy')
     number_of_particles = 50
     Xt_1 = generate_particles(number_of_particles, m)
-    xt_list = [[200, 500, np.pi/2], [200, 450, np.pi/2], [200, 400, np.pi/2], [200, 400, np.pi], [150, 400, np.pi], [100, 400, np.pi]]
-    ut = [[50, 0], [50, 0], [50, 0], [0, np.pi/2], [50, 0], [50, 0]]
+    xt_list = [[300, 300, np.pi/2], [300, 275, np.pi/2], [300, 250, np.pi/2], [300, 225, np.pi/2]]
+    ut = [[25, 0], [25, 0], [25, 0], [25, 0]]
     zt = find_z_values(xt_list, m)
 
+    positions1 = np.array([sublist[0] for sublist in Xt_1])
+    x_positions1 = positions1[:, 0]
+    y_positions1 = positions1[:, 1]
+    
+    plt.title("MCL")
+    plt.imshow(m, cmap='Greys')
+    plt.scatter(x_positions1, y_positions1, c='r', marker='.')
+    plt.scatter(xt_list[0][0], xt_list[0][1], c='g', marker='o')
+    plt.savefig(f"images/step_{0}.png")
+    list_of_plots.append(f"images/step_{0}.png")
+    plt.close()
+
     for i, utt in enumerate(ut):
+        print(f"Step {i}")
         Xt = mrl2.MCL(Xt_1, utt, zt[i], m)
         positions = np.array([sublist[0] for sublist in Xt])
         x_positions = positions[:, 0]
         y_positions = positions[:, 1]
-        weights = [sublist[1] for sublist in Xt]
-        positions1 = np.array([sublist[0] for sublist in Xt_1])
-        x_positions1 = positions1[:, 0]
-        y_positions1 = positions1[:, 1]
 
-        plt.title("Sample landmark model")
-        plt.imshow(np.load('map/binary_image.npy'), cmap='Greys')
-        plt.scatter(x_positions, y_positions, c='b', marker='.')
-        plt.scatter(x_positions1, y_positions1, c='r', marker='o')
+        plt.title("MCL ")
+        plt.imshow(m, cmap='Greys')
+        plt.scatter(x_positions, y_positions, c='r', marker='.')
         plt.scatter(xt_list[i][0], xt_list[i][1], c='g', marker='o')
-        plt.savefig(f"images/step_{i}.png")
-        list_of_plots.append(f"images/step_{i}.png")
+        plt.savefig(f"images/step_{i+1}.png")
+        list_of_plots.append(f"images/step_{i+1}.png")
         plt.close()
         Xt_1 = Xt
 
     return list_of_plots
 
+
+def Augmented_MCL_test():
+    list_of_plots = []
+    m = np.load('map/binary_image_cats.npy')
+    number_of_particles = 50
+    Xt_1 = generate_particles(number_of_particles, m)
+    xt_list = [[400, 500, np.pi/2], [400, 450, np.pi/2], [400, 400, np.pi/2], [400, 350, np.pi/2], [400, 300, np.pi/2], [400, 250, np.pi/2]]
+    ut = [[50, 0], [50, 0], [50, 0], [50, 0], [50, 0], [50, 0]]
+    zt = find_z_values(xt_list, m)
+
+    positions1 = np.array([sublist[0] for sublist in Xt_1])
+    x_positions1 = positions1[:, 0]
+    y_positions1 = positions1[:, 1]
+    
+    plt.title("Augmented_MCL")
+    plt.imshow(m, cmap='Greys')
+    plt.scatter(x_positions1, y_positions1, c='r', marker='.')
+    plt.scatter(xt_list[0][0], xt_list[0][1], c='g', marker='o')
+    plt.savefig(f"images/step_{0}.png")
+    list_of_plots.append(f"images/step_{0}.png")
+    plt.close()
+
+    for i, utt in enumerate(ut):
+        print(f"Step {i}")
+        Xt = mrl2.Augmented_MCL(Xt_1, utt, zt[i], m)
+        positions = np.array([sublist[0] for sublist in Xt])
+        x_positions = positions[:, 0]
+        y_positions = positions[:, 1]
+        weights = [sublist[1] for sublist in Xt]
+
+        plt.title("Augmented_MCL ")
+        plt.imshow(m, cmap='Greys')
+        plt.scatter(x_positions, y_positions, c='r', marker='.')
+        plt.scatter(xt_list[i][0], xt_list[i][1], c='g', marker='o')
+        plt.savefig(f"images/step_{i+1}.png")
+        list_of_plots.append(f"images/step_{i+1}.png")
+        plt.close()
+        Xt_1 = Xt
+
+    return list_of_plots
+
+def KLD_Sampling_MCL_test():
+    list_of_plots = []
+    m = np.load('map/binary_image_cats.npy')
+    number_of_particles = 50
+    Xt_1 = generate_particles(number_of_particles, m)
+    xt_list = [[680, 500, np.pi/2], [680, 450, np.pi/2], [680, 400, np.pi/2], [680, 350, np.pi/2], [680, 300, np.pi/2], [680, 250, np.pi/2]]
+    ut = [[50, 0], [50, 0], [50, 0], [50, 0], [50, 0], [50, 0]]
+    zt = find_z_values(xt_list, m)
+    epsilon = 0.1
+    delta = 0.05
+
+    positions1 = np.array([sublist[0] for sublist in Xt_1])
+    x_positions1 = positions1[:, 0]
+    y_positions1 = positions1[:, 1]
+    
+    plt.title("KLD_Sampling_MCL")
+    plt.imshow(m, cmap='Greys')
+    plt.scatter(x_positions1, y_positions1, c='r', marker='.')
+    plt.scatter(xt_list[0][0], xt_list[0][1], c='g', marker='o')
+    plt.savefig(f"images/step_{0}.png")
+    list_of_plots.append(f"images/step_{0}.png")
+    plt.close()
+
+    for i, utt in enumerate(ut):
+        print(f"Step {i}")
+        Xt = mrl2.KLD_Sampling_MCL(Xt_1, utt, zt[i], m, epsilon, delta)
+        positions = np.array([sublist[0] for sublist in Xt])
+        x_positions = positions[:, 0]
+        y_positions = positions[:, 1]
+
+        plt.title("KLD_Sampling_MCL ")
+        plt.imshow(m, cmap='Greys')
+        plt.scatter(x_positions, y_positions, c='r', marker='.')
+        plt.scatter(xt_list[i][0], xt_list[i][1], c='g', marker='o')
+        plt.savefig(f"images/step_{i+1}.png")
+        list_of_plots.append(f"images/step_{i+1}.png")
+        plt.close()
+        Xt_1 = Xt
+
+    return list_of_plots
+
+def test_range_measurement_test():
+    m = np.load('map/binary_image_cats.npy') 
+    Xbar_t = [[400, 500, np.pi/2], [400, 450, np.pi/2], [400, 400, np.pi/2], [400, 350, np.pi/2], [400, 300, np.pi/2], [400, 250, np.pi/2]]
+    zt = find_z_values_1(Xbar_t, m)
+    x = 10
+    print(mrl2.test_range_measurement(zt, Xbar_t, m , x))
+
 if __name__ == "__main__":
-    #Grid_localization_test()
+    """
+    See image: images/grid_localization.png for results
+    """
+    Grid_localization_test()
+
+    """
+    See GIF: images/animation_MCL_.gif for results
+    """
     #MCL_test()
-    create_MCL_GIF()
+    create_GIF(MCL_test())
+
+    """
+    See GIF: images/animation_Augmented_MCL.gif for results
+    """
+    #Augmented_MCL_test()
+    create_GIF(Augmented_MCL_test())
+
+    """
+    See GIF: images/animation_KLD_Sampling_MCL.gif for results
+    """
+    #KLD_Sampling_MCL_test()
+    create_GIF(KLD_Sampling_MCL_test())
+
+    """
+    Prints out results of test_range_measurement
+    """
+    test_range_measurement_test()
